af6ce17291d43ddb437e6d29eb7978cc66e4a4fd
fernandoleonett <fernandoleonett@gmail.com>
2023-04-18 17:13:57 +0200

    docker compose working bakend, persitency and  seed

--- src/connection/connectiion.ts
+++ src/connection/connectiion.ts
@@ -3,7 +3,8 @@ import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose'
 export class DatabaseConnection implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
     return {
-      uri: process.env.CONEXION,
+      // uri:"mongodb://127.0.0.1:27017/nestjs",
+      uri: process.env.MONGODB_URL,
       useNewUrlParser: true,
     };
   }
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cors from 'cors';
 
 
-async function generateSwagger(app: INestApplication) {
-  const options = new DocumentBuilder()
-    .setTitle('API de tareas')
-    .setDescription('API para crear, actualizar, borrar y listar tareas')
-    .setVersion('1.0')
-    .addTag('tareas')
-    .addServer('/api')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
-}
+// async function generateSwagger(app: INestApplication) {
+//   const options = new DocumentBuilder()
+//     .setTitle('API de tareas')
+//     .setDescription('API para crear, actualizar, borrar y listar tareas')
+//     .setVersion('1.0')
+//     .addTag('tareas')
+//     .addServer('/api')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api-docs', app, document);
+// }
 
 
 async function bootstrap() {
@@ -25,7 +25,7 @@ async function bootstrap() {
   const app = await NestFactory.create(TasksModule);
   app.use(cors()); // habilitar CORS
   app.useGlobalPipes(new ValidationPipe())
-  await generateSwagger(app); // llamada a la función generateSwagger
+  // await generateSwagger(app); // llamada a la función generateSwagger
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
